0c14a4c8a15ee6e23dd134e78ef62a13e852d3b2
crosshj <github@chimpjuice.com>
2021-04-28 23:32:55 -0400

    terminal gets service name from query params

--- modules/Terminal.mjs
+++ modules/Terminal.mjs
@@ -3,6 +3,7 @@ import {
 	connectTrigger,
 	execCommand,
 } from "./terminalEvents.mjs";
+import { getCurrentService } from './state.mjs';
 import { debounce } from "/shared/modules/utilities.mjs";
 import motd from "./motd.mjs";
 
@@ -26,7 +27,7 @@ function _Terminal() {
 	termContainer.innerHTML = `
 <iframe
 	sandbox="${iframeSandboxPermissions}"
-	src="/_/modules/terminal/index.html"
+	src="/_/modules/terminal/index.html?service='${(getCurrentService()||{}).name}'"
 ></iframe>
 `.trim();
 
