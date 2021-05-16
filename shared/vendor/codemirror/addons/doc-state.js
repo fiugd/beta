ad4e7124f7ed0dc6542f17e12cd0fa99cc7fc9bf
crosshj <github@chimpjuice.com>
2021-05-16 19:54:27 -0400

    search fix

--- shared/vendor/codemirror/addons/doc-state.js
+++ shared/vendor/codemirror/addons/doc-state.js
@@ -128,6 +128,7 @@ further reference, see defineExtension here https://codemirror.net/doc/manual.ht
 
 	const selectLine = (cm, doc, line, ch) => {
 		const newLine = ch ? { line, ch } : line;
+		doc.focus();
 		doc.setCursor(newLine);
 		const t = doc.cm.charCoords(newLine, "local").top;
 		cm.scrollTo(0, t - SCROLL_MARGIN);
