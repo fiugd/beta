cad7bb10eb3ef73cc552b25fb35dfa7b6bcf0446
crosshj <github@chimpjuice.com>
2021-05-25 21:51:58 -0400

    node operation: trying to make worker exit work

--- terminal/.example.js
+++ terminal/.example.js
@@ -1,8 +1,21 @@
 import { sleep } from './.example_import.js';
 
-(async () => {
-	console.log('start execution');
+const these = [
+	['one', 5000],
+	['two', 1000],
+	['three', 300],
+];
+
+const delay = (time) => new Promise((resolve)=> setTimeout(resolve, time) );
+
+const AsyncTask = async (item) => {
+	const [name, time] = item;
+	console.log(`start execution ${name}`);
 	//throw new Error('error test');
-	sleep(3000);
-	console.log('end execution');
-})();
+	sleep(time); //<< will block other threads from starting
+	//await delay(time); //will allow worker to exit since it's a microtask... sigh
+	console.log(`end execution ${name}`);
+};
+
+Promise.all(these.map(AsyncTask));
+//AsyncTask(these[0]);
