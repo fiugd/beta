3a6570783bfa73b354507f0acf1a763419a4b669
crosshj <github@chimpjuice.com>
2021-05-27 01:10:46 -0400

    node op: when does script exit?

--- terminal/terminal.ops.test.js
+++ terminal/terminal.ops.test.js
@@ -1,4 +1,33 @@
 //show-preview
+
+/*
+
+I'm thinking there may be a way to guess if script is not active in worker - will try that
+
+*/
+
+/*
+import GetDynamicOps from './terminal.ops.dynamic.js';
+import {parseArgs} from './terminal.lib.js';
+
+const term = {
+	write: console.log
+};
+const comm = () => {};
+const getCwd = () => 'crosshj/fiug-beta/terminal';
+const logger = console.log;
+const done = () => console.log('finished');
+
+GetDynamicOps(term, comm, getCwd)
+	.then(ops => {
+		const thisOp = ops[1];
+		const args = parseArgs(thisOp, '.example.js');
+		console.log(thisOp)
+		thisOp.invoke(args, done);
+	});
+*/
+
+
 (async () => {
 	const baseUrl = location.origin+ '/crosshj/fiug-beta/terminal';
 	const { default: GetDynamicOps } = await import(`${baseUrl}/terminal.ops.dynamic.js`);
