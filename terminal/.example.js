3e942250a8abe8a440a1975fc19428da26e7d445
crosshj <github@chimpjuice.com>
2021-05-27 23:10:52 -0400

    node op: wait for done in async code

--- terminal/.example.js
+++ terminal/.example.js
@@ -1,21 +1,27 @@
-import { sleep } from './.example_import.js';
-
+//show-preview
+//import { sleep } from './.example_import.js';
 const these = [
 	['one', 5000],
 	['two', 1000],
 	['three', 300],
 ];
 
-const delay = (time) => new Promise((resolve)=> setTimeout(resolve, time) );
+const delay = (time) => new Promise((resolve)=> setTimeout(() => resolve('done'), time) );
 
 const AsyncTask = async (item) => {
 	const [name, time] = item;
 	console.log(`start execution ${name}`);
 	//throw new Error('error test');
-	sleep(time); //<< will block other threads from starting
-	//await delay(time); //will allow worker to exit since it's a microtask... sigh
+	//sleep(time); //<< will block other threads from starting
+	await delay(time); //will allow worker to exit since it's a microtask... sigh
 	console.log(`end execution ${name}`);
-};
+}
 
-Promise.all(these.map(AsyncTask));
-//AsyncTask(these[0]);
+const mapTasks = () => these.map(async (item) => await AsyncTask(item));
+
+console.log('start');
+(async () => {
+	//await Promise.all(mapTasks());
+	await AsyncTask(these[0]);
+	console.log('done');
+})();
--- terminal/bin/node.js
+++ terminal/bin/node.js
@@ -6,21 +6,46 @@ const operation = async (args) => {
 	const scriptUrl = `${location.origin}/${cwd}/${file}`;
 	const scriptText = await (await fetch(scriptUrl)).text();
 
+	function AsyncHooked(original){
+		const replaced = `self.asyncHooks[self.hookCount++] = `;
+		return original
+			//.replace(/async /gm, replaced + 'async ')
+			.replace(/^[\s\t]*await /gm, '' + replaced);
+	}
+
 	const runScript = (name, src, logger) => new Promise((resolve, reject) => {
 		const workerSrc = `
 			console.log = (...args) => postMessage({ log: args });
 			console.warn = console.info = console.log;
 			console.error = (error) => postMessage({ error });
+			self.asyncHooks = [];
+			self.hookCount = 0;
 
-			${src}
+			${AsyncHooked(src)}
 
-			queueMicrotask(() => {
-				postMessage({ exit: true });
-			});
+			//queueMicrotask(() => {
+			//	postMessage({ exit: true });
+			//});
+			setTimeout(async () => {
+				//console.log(self.asyncHooks.length);
+				await Promise.allSettled(self.asyncHooks);
+				//console.log(self.asyncHooks.length);
+				queueMicrotask(() => {
+					postMessage({ exit: true });
+				});
+			}, 1);
 		`.replace(/^			/gm, '')
 		.replace(/from \'\./gm, `from '${location.origin}/${cwd}`)
 		.replace(/from \"\./gm, `from "${location.origin}/${cwd}`)
-		.trim();
+		.trim()
+		.split('\n')
+		.map(line => {
+			if(!line.includes('self.asyncHooks[self.hookCount++] =')) return line;
+			return line+`\nawait self.asyncHooks[self.hookCount-1];`
+		})
+		.join('\n');
+
+		console.log(workerSrc)
 
 		const blob = new Blob([ workerSrc ], { type: "text/javascript" });
 		const type = 'module';
@@ -33,7 +58,7 @@ const operation = async (args) => {
 		const worker = new Worker(URL.createObjectURL(blob), { name, type });
 		const exitWorker = () => { worker.terminate(); resolve(); };
 		worker.onerror = (error) => {
-			console.log(error.message || 'unknown error');
+			console.error(error.message || 'unknown error');
 			exitWorker();
 		};
 		worker.onmessage = (e) => {
