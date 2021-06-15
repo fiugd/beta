5a72de5055a72704ba1817c18a53eda7b80952f3
crosshj <github@chimpjuice.com>
2021-06-15 11:49:22 -0400

    preview

--- terminal/bin/preview.js
+++ terminal/bin/preview.js
@@ -18,9 +18,11 @@ const operation = async (args, done) => {
 			<style>
 				#preview-container {
 					position: absolute; left:0; right:0; top:0; bottom:0;
+					z-index: 999;
 				}
 				#preview-container iframe {
 					position: absolute; left:0; top:0; width: 100%; height: 100%
+					border: 0;
 				}
 				.hidden { display: none; }
 				#quit-preview {
