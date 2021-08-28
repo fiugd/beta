bf4d8eebb5fccee2c33c05ff0cc7dbba93b580dd
crosshj <github@chimpjuice.com>
2021-08-28 02:30:05 -0400

    git clone - hit a roadblock

--- .welcome/1ncubate/git.clone.js
+++ .welcome/1ncubate/git.clone.js
@@ -11,22 +11,49 @@ GOALS:
 
 - do everything this does (lol) https://cli.github.com/
 
+BLOCKER:
+- can't access sessionStorage in worker, need it for github auth
+
+SOLUTIONS:
+? 1) beef up communication between worker and main page, expose sessionStorage
+* 2) read/write file (with token in it) using serviceWorker, implement .gitignore so this file isn't checked in
+* 3) service worker can read github auth and use it
+
+2 & 3 can be seen as the same solution give or take the .gitignore part
+
+fetch("https://beta.fiug.dev/service/change", {
+	"body": "{\n  \"path\": \"./crosshj/fiug-beta/secrets.json\",\n  \"code\": \"{}\\n\",\n  \"service\": \"crosshj/fiug-beta\"\n}",
+	"method": "POST",
+});
+
+fetch("https://beta.fiug.dev/crosshj/fiug-beta/secrets.json", {
+	"body": "{\n  \"path\": \"./crosshj/fiug-beta/secrets.json\",\n  \"code\": \"{}\\n\",\n  \"service\": \"crosshj/fiug-beta\"\n}",
+	"method": "GET",
+});
+
+https://github.com/nodeca/js-yaml
+https://github.com/eemeli/yaml
+
+https://codemirror.net/mode/yaml/index.html
+
 */
 
-const cloneUrl = 'https://beta.fiug.dev/service/create/provider';
-const bodyObj = {
-	"providerType":"github-provider",
-	"auth": "",
-	"repo": "crosshj/fiug-beta",
-	"branch":"main",
-	"operation":"provider-add-service"
-};
-const body = JSON.stringify(bodyObj);
-const method = 'POST';
+try {
+	const auth = ''; //sessionStorage.getItem('Github Personal Access Token');
 
-(async () => {
+	const cloneUrl = 'https://beta.fiug.dev/service/create/provider';
+	const bodyObj = {
+		providerType:"github-provider",
+		auth,
+		repo: "crosshj/footils",
+		branch:"master",
+		operation:"provider-add-service"
+	};
+	const body = JSON.stringify(bodyObj);
+	const method = 'POST';
 
 	console.log(`\nCloning ${bodyObj.repo}, ${bodyObj.branch} branch...`)
+
 	const { result } = await fetch(cloneUrl, { body, method }).then(x=>x.json());
 	const { services } = result;
 	const [ cloned ] = services;
@@ -34,5 +61,7 @@ const method = 'POST';
 	//console.log(JSON.stringify(cloned, null, 2));
 	//console.log(clone.name);
 	console.log(`DONE`);
+} catch(e){
+	console.log(`ERROR: ${e.message}`)
+}
 
-})();
