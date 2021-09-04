f0d289cd2a93aa8700c5f628e85351ed31438072
crosshj <github@chimpjuice.com>
2021-09-04 06:05:27 -0400

    git clone: the brunt of what needs done

--- modules/service-worker.storage.js
+++ modules/service-worker.storage.js
@@ -447,7 +447,7 @@
 				for( var i = 0; i < binary.length; i++ ) {
 					array[i] = binary.charCodeAt(i)
 				}
-				return new Blob([array]);
+				return new Blob([array], { type: "image/png" });
 			};
 
 			const Storeable = (path, content) => {
