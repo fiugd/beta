f495dfaa0b682939683d0a58bdb00c7372d4328d
crosshj <github@chimpjuice.com>
2021-09-08 15:04:44 -0400

    refactoring file org

---
+++ service-worker/_build.js
@@ -0,0 +1,12 @@
+/*
+
+would like to bundle the service worker into one file and not have to use bastardized module.exports/require
+
+https://parcel-repl.vercel.app/
+from https://github.com/parcel-bundler/parcel/issues/1253
+
+https://skalman.github.io/UglifyJS-online/
+
+
+*/
+
