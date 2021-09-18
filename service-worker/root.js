e96b9ad7a0264cbb53e0bc196d1907c400f2b5f5
crosshj <github@chimpjuice.com>
2021-09-18 19:17:24 -0400

    root service

--- service-worker/root.js
+++ service-worker/root.js
@@ -20,7 +20,8 @@ const initRootService = async ({ stores }) => {
 # https://phoenixnap.com/kb/change-bash-prompt-linux
 # http://bashrcgenerator.com/
 
-# in the future, parse this and use for prompt` + 
+# in the future, parse this and use for prompt
+` + 
 'export PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\n\\$ \[$(tput sgr0)\]"'
 + `
 
@@ -29,19 +30,30 @@ const initRootService = async ({ stores }) => {
 	await files.setItem("~/welcome", `
 Welcome to fiug!
 
-Here are some tips and examples.
+Here are some tips and examples.  Try these in the terminal to the right.
 
 Clone a repo:
 git clone crosshj/fiug-welcome
 
+Use git config to set your name, email, ang github token.
+(OUT OF ORDER)
+
+(OUT OF ORDER)
+List all cloned repos:
+git list
+
+(OUT OF ORDER)
 Open a repo in editor:
 git open crosshj/fiug-welcome
 
-View all changed files:
+View names of changed files:
 git status
 
 View changes:
 git diff
+
+View changes in a specific file:
+git diff README.md
 
 Create and push a commit to github:
 git commit -m "message about changes"
@@ -49,13 +61,13 @@ git commit -m "message about changes"
 Download all templates for preview:
 git clone crosshj/fiug-plugins
 
-This previews a file
+Previews files:
 preview *.*
 
+Preview a specific file:
+preview README.md
+
 Click on preview and hit Control to show a quit button.
-
-Use git config to set your name, email, ang github token.
-
 
 `.trim() +'\n');
 
