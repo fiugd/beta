ed75e38269c4020051b652757eea3ad86cb00eae
crosshj <github@chimpjuice.com>
2021-09-27 23:14:35 -0400

    troubleshooting sw build

--- service-worker/root.profile.js
+++ service-worker/root.profile.js
@@ -1,11 +1,9 @@
-export const profile = () => (`
-
+export const profile = () => `
 # configure prompt here
 # https://phoenixnap.com/kb/change-bash-prompt-linux
 # http://bashrcgenerator.com/
 
 # in the future, parse this and use for prompt
-` + 
-'export PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\n\\$ \[$(tput sgr0)\]"'
-
-).trim() +'\n';
+`.trim() + 
+'export PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\n\\$ \[$(tput sgr0)\]"' +
+`\n`;
