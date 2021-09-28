baa973e00bcabe2279b97ba3da4ca1bd39e82500
crosshj <github@chimpjuice.com>
2021-09-27 22:59:27 -0400

    service worker root: break out root files into separate

--- service-worker/root.js
+++ service-worker/root.js
@@ -1,3 +1,6 @@
+import { settings } from './root.settings.js';
+import { welcome } from './root.welcome.js';
+
 const initRootService = async ({ stores }) => {
 	const {services, files, changes} = stores;
 	const service = {
@@ -12,69 +15,11 @@ const initRootService = async ({ stores }) => {
 		}},
 	};
 	await services.setItem('0', service);
+
 	await files.setItem("~/.git/config", '\n');
-	await files.setItem("~/settings.json", '{}');
-
-	await files.setItem("~/.profile", `
-# configure prompt here
-# https://phoenixnap.com/kb/change-bash-prompt-linux
-# http://bashrcgenerator.com/
-
-# in the future, parse this and use for prompt
-` + 
-'export PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\n\\$ \[$(tput sgr0)\]"'
-+ `
-
-`.trim() +'\n');
-
-	await files.setItem("~/welcome.md", `
-Welcome to fiug!
-================
-
-Try out the terminal on the right.
-
-#### configure git:
-\`git config --global user.name john\`
-\`git config --global user.email johndoe@example.com\`
-\`git config --global user.token {your github token}\`
-
-#### clone a repo:
-\`git clone crosshj/fiug-welcome\`
-
-#### list all cloned repos:
-\`git list\`
-
-#### open/close a repo in editor:
-\`git open crosshj/fiug-welcome\`
-\`git close\`
-
-#### view names of changed files:
-\`git status\`
-
-#### view changes:
-\`git diff\`
-
-#### view changes in a specific file:
-\`git diff README.md\`
-
-#### create and push a commit to github:
-\`git commit -m "message about changes"\`
-
-#### download all templates (for preview):
-\`git clone crosshj/fiug-plugins\`
-
-#### preview files:
-\`preview\`
-
-#### preview a specific file:
-\`preview README.md\`
-
-#### quit preview
-1. click preview pane
-2. press Control
-3. click quit
-
-`.trim() +'\n');
+	await files.setItem("~/settings.json", settings());
+	await files.setItem("~/.profile", profile());
+	await files.setItem("~/welcome.md", welcome());
 
 	await changes.setItem(`state-${service.name}-opened`, [
 		{ name: 'welcome.md', order: 0 }
