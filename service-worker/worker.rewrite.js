1016e46ca141ba9814d09f2953ec162639ee4700
crosshj <github@chimpjuice.com>
2021-09-28 18:01:22 -0400

    worker rewrite

--- service-worker/worker.rewrite.js
+++ service-worker/worker.rewrite.js
@@ -1,12 +1,19 @@
+//import Babel from 'https://cdn.skypack.dev/@babel/standalone';
+import Babel from 'https://cdn.skypack.dev/-/@babel/standalone@v7.15.7-1HPSIsmADpc5jJR5wUwi/dist=es2020,mode=imports,min/optimized/@babel/standalone.js';
+
 async function getHandler(args){
 	const { stores } = this;
 	const { path, query } = args;
 
 	const content = await stores.files.getItem(path);
+	var output = Babel.transform(exampleSrc, {
+		//plugins: ['importMap', 'console', 'processExit'],
+		//sourceType: "module"
+	});
 
 	return JSON.stringify({
 		stores: Object.keys(stores),
-		path, query, content
+		path, query, output
 	}, null, 2);
 }
 
