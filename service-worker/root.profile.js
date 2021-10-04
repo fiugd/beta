e1988c9f802ac4bb24a967353afe53086b4d20e1
crosshj <github@chimpjuice.com>
2021-10-04 02:18:10 -0400

    more prompt action

--- service-worker/root.profile.js
+++ service-worker/root.profile.js
@@ -4,7 +4,11 @@ export const profile = () => {
 # https://phoenixnap.com/kb/change-bash-prompt-linux
 # http://bashrcgenerator.com/
 
-# in the future, parse this and use for prompt
+# [\\033[38;2;{R};{B};{B}m <-- RGB foreground ON
+# [\\033[39m               <-- RGB foreground OFF
+# [\\033[37m[\\033[1m       <-- white, bold
+# [\\033[0m                <-- all styles off
+
 `.trim() + 
 '\nexport PS1="[\\033[38;2;60;180;190m\\h[\\033[39m [\\033[38;2;0;255;0m\\W[\\033[39m\\n[\\033[37m[\\033[1m\\$ [\\033[0m"' +
 '\n';
