2e0645b1ae50b96412b2c2d4fc7cc5a1881dddb8
crosshj <github@chimpjuice.com>
2021-10-04 00:49:46 -0400

    service worker build

--- service-worker/root.profile.js
+++ service-worker/root.profile.js
@@ -6,6 +6,6 @@ export const profile = () => {
 
 # in the future, parse this and use for prompt
 `.trim() + 
-	'\nexport PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\n\\$ \[$(tput sgr0)\]"' +
+	'\nexport PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\\n\\$ \[$(tput sgr0)\]"' +
 	`\n`;
 }
