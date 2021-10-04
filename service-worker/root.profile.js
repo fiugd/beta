59a56367e56ee662b61ea7fb24eda3afa41895dd
crosshj <github@chimpjuice.com>
2021-10-04 01:57:21 -0400

    .profile: a sample prompt that works

--- service-worker/root.profile.js
+++ service-worker/root.profile.js
@@ -6,6 +6,6 @@ export const profile = () => {
 
 # in the future, parse this and use for prompt
 `.trim() + 
-	'\nexport PS1="\[\\033[38;5;14m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\\033[38;5;2m\]\W\[$(tput sgr0)\]\\n\\$ \[$(tput sgr0)\]"' +
-	`\n`;
+'\nexport PS1="[\\033[38;2;60;180;190m\h[\\033[39m [\\033[38;2;0;255;0m\W[\\033[39m\n[\\033[37m[\\033[1m\$ [\\033[0m"' +
+'\n';
 }
