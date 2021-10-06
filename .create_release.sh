29327b22dd2f161c6459bee8107d2e23de06643e
crosshj <github@crosshj.com>
2021-10-06 14:15:08 -0400

    Update .create_release.sh

--- .create_release.sh
+++ .create_release.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-mkdir .release
 
-cp -r ./ ./.release
+mkdir /tmp/.release
+cp -r ./ /tmp/.release
+
+mkdir .release
+cp -r /tmp/.release ./.release
 
 echo "Bash script thinks version is: $1"
 
-# would copy all files for release here
