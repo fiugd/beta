9cc359c92398ff2833c0fb821826bfc815b1da41
crosshj <github@crosshj.com>
2021-10-06 14:35:06 -0400

    Update .create_release.sh

--- .create_release.sh
+++ .create_release.sh
@@ -9,6 +9,9 @@ cp -vr /tmp/.release ./
 
 rm -rf ./.release/.git
 rm -rf ./.release/.github
+rm -rf ./.release/.create_release.sh
 
+echo "fiug.dev" > ./.release/CNAME
+
+# TODO: verify here that package.json version matches version passed to this bash script
 echo "Bash script thinks version is: $1"
-
