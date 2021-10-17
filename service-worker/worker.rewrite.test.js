2c1b7e56f194d9bbc1437edb3ab8259a778fdf5c
crosshj <github@chimpjuice.com>
2021-10-17 05:22:53 -0400

    trying to figure out why SW build not working on windows machine

--- service-worker/worker.rewrite.test.js
+++ service-worker/worker.rewrite.test.js
@@ -2,18 +2,18 @@ import { transpile, WorkerRewrite } from './worker.rewrite.js';
 
 const content = `
 import 'rollup';
-import 'source-map';
+import 'sourceMap';
 import 'terser';
 
-import rollupConfig from 'https://beta.fiug.dev/crosshj/fiug-beta/service-worker/build/rollup.config.js';
-import terserConfig from 'https://beta.fiug.dev/crosshj/fiug-beta/service-worker/build/terser.config.js';
-import packageJson from "https://beta.fiug.dev/crosshj/fiug-beta/package.json" assert { type: "json" };
+import rollupConfig from '/crosshj/fiug-beta/service-worker/build/rollup.config.js';
+import terserConfig from '/crosshj/fiug-beta/service-worker/build/terser.config.js';
+import packageJson from "/crosshj/fiug-beta/package.json" assert { type: "json" };
 `;
 const map = {
 	imports: {
 		rollup: 'https://unpkg.com/rollup/dist/rollup.browser.js',
-		'source-map': 'https://cdn.jsdelivr.net/npm/source-map@0.7.3/dist/source-map.js',
-		terser: 'https://cdn.jsdelivr.net/npm/terser/dist/bundle.min.js'
+		sourceMap: 'https://cdn.jsdelivr.net/npm/source-map@0.7.3/dist/source-map.js',
+		terser: 'https://cdn.jsdelivr.net/npm/terser/dist/bundle.min.js',
 	}
 };
 
