a8a82ea3a7acbbf2edb53c0a495d23e08673d181
crosshj <github@chimpjuice.com>
2021-10-19 15:09:29 -0400

    terminal module cleanup

--- modules/Terminal.mjs
+++ modules/Terminal.mjs
@@ -10,6 +10,28 @@ const iframeSandboxPermissions = [
 ].join(' ');
 
 const termContainerSrc = `
+<style>
+	.term-contain {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0px;
+		bottom: 0px;
+		overflow: hidden;
+	}
+	#terminal .term-contain { z-index: 999; }
+	#terminal iframe {
+		position: relative;
+		top: 0;
+		right: -1px;
+		left: -1px;
+		bottom: -1px;
+		width: calc(100% + 5px);
+		height: 100%;
+		z-index: 100;
+		border: 0px;
+	}
+</style>
 <iframe
 	sandbox="${iframeSandboxPermissions}"
 	src="/_/modules/terminal/index.html"
