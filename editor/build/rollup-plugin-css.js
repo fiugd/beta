706764fc433e6ae9355bd3ca0c60249ef31e9ab2
crosshj <github@chimpjuice.com>
2021-10-30 02:50:33 -0400

    editor in its own iframe: include mimes in bundle

--- editor/build/rollup-plugin-css.js
+++ editor/build/rollup-plugin-css.js
@@ -1,6 +1,10 @@
 
 /*
 see json plugin and following url for info on writing this plugin
-
 https://github.com/calebdwilliams/rollup-plugin-import-assert/blob/main/src/import-assert.ts
+
+
+more resources for plugins:
+https://unpkg.com/browse/@rollup/pluginutils@4.1.1/dist/es/index.js
+
 */
