5261145b7bb40c63addb237735f7e992d23f5721
crosshj <github@chimpjuice.com>
2022-01-10 23:18:56 -0500

    Update 404.js

--- 404.js
+++ 404.js
@@ -5,7 +5,7 @@ const loadRepo = (path) => {
 	console.log('load repo: '+path)
 };
 const onload = () => {
-	if(window.parent) return notFound();
+	if(window === window.parent) return notFound();
 	const path = document.location.pathname.slice(1);
 	const isRepoPath = new RegExp('^[a-zA-Z]*\/[a-zA-Z]*$').test(path)
 	if(!isRepoPath) return notFound();
