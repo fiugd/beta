9d40313a0b2a511c26cc3a7d971f2ec158e0c5f7
crosshj <github@chimpjuice.com>
2022-01-10 23:12:23 -0500

    hijack 404

---
+++ 404.js
@@ -0,0 +1,15 @@
+const notFound = () => {
+	document.body.innerHTML = 'not found';
+};
+const loadRepo = (path) => {
+	console.log('load repo: '+path)
+};
+const onload = () => {
+	if(window.top) return notFound();
+	const path = document.location.pathname.slice(1);
+	const isRepoPath = new RegExp('^[a-zA-Z]*\/[a-zA-Z]*$').test(path)
+	if(!isRepoPath) return notFound();
+	loadRepo(path);
+};
+
+window.onload = onload;
